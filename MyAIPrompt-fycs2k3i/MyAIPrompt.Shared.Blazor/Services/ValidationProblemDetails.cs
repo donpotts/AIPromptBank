726645141f7a4c1a0b932990b32e0b647fac44e2
Don Potts <don.potts@beverycreative.com>
2025-08-09 09:00:23 -0500

    Removed other project

--- MyAIPrompt-fycs2k3i/MyAIPrompt.Shared.Blazor/Services/ValidationProblemDetails.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace MyAIPrompt.Shared.Blazor.Services;
-
-internal class ValidationProblemDetails : ProblemDetails
-{
-    [JsonPropertyName("errors")]
-    public Dictionary<string, List<string>>? Errors { get; set; }
-}
--- MyAIPrompt-fycs2k3i/MyAIPrompt.Shared/Models/ApplicationUserWithRolesDto.cs
+++
@@ -1,8 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace MyAIPrompt.Shared.Models;
-
-public class ApplicationUserWithRolesDto : ApplicationUserDto
-{
-    public List<string>? Roles { get; set; }
-}
